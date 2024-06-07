0856f254c344db58414381d7d212d67f7f14dfdd
Juan Diego Cueto Dominguez <u202012207@upc.edu.pe>
2024-06-07 14:01:07 -0500

    refactor: removed unused annotations.

--- src/main/java/com/acme/backend/fithubpro/iam/domain/model/queries/GetUserQuery.java
+++ src/main/java/com/acme/backend/fithubpro/iam/domain/model/queries/GetUserQuery.java
@@ -1,13 +1,12 @@
 package com.acme.backend.fithubpro.iam.domain.model.queries;
 
-import lombok.Getter;
 
 /**
  * @summary The GetUserQuery class represents a query to get a user by username.
  * The query contains the username of the user.
  */
 
-@Getter
+
 public record GetUserQuery(String username) {
 
 }
